92a47714ebaec68fca8b8d0a85cd9217ca41941c
Gary Fernie <g@ryfernie.me>
2015-03-11 13:20:47 +0000

    Add TrendingTopics class
    - Corresponding unit tests

---
+++ GroupA.FolksOpinion.UI.UnitTests/TrendingTopicsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroupA.FolksOpinion.UI.Models;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Caching;
+
+namespace GroupA.FolksOpinion.UI.UnitTests
+{
+    [TestClass]
+    public class TrendingTopicsTests
+    {
+        [TestClass]
+        public class TrendingTopicsConstructor
+        {
+            [TestMethod]
+            public void TrendingTopicsConstructorDefault()
+            {
+                new TrendingTopics();
+            }
+        }
+
+        [TestClass]
+        public class CurrentTrendsProperty
+        {
+            [TestMethod]
+            public void CurrentTrendsPropertyGet()
+            {
+                var t = new TrendingTopics();
+                var c = t.CurrentTrends;
+            }
+        }
+    }
+}
---
+++ GroupA.FolksOpinion.UI/Models/TrendingTopics.cs
@@ -0,0 +1,32 @@
+﻿/* File:        TrendingTopics.cs
+ * Purpose:     Encapsulates trending topics state and functionality.
+ * Created:     9th March 2015
+ * Author:      Gary Fernie
+ * Exposes:     TrendingTopics
+ * 
+ * Description: - Represents a list of trending topics.
+ *              - Provides functionality to maintin list.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupA.FolksOpinion.UI.Models
+{
+    public class TrendingTopics
+    {
+        private static FolksOpinionTwitterApi api = FolksOpinionTwitterApi.Instance;
+        
+        public IEnumerable<Trend> CurrentTrends
+        {
+            get { return GetTrends(); }
+        }
+
+        private IEnumerable<Trend> GetTrends()
+        {
+            return api.GetTrends();
+        }
+    }
+}
