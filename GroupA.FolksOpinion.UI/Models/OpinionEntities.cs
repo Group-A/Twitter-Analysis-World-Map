0d68f8d78fb263640e3b3f8c45ea6b585dd44a82
Gary Fernie <g@ryfernie.me>
2015-03-24 21:21:36 +0000

    Add new empty classes

--- GroupA.FolksOpinion.UI/Models/OpinionEntities.cs
+++ GroupA.FolksOpinion.UI/Models/OpinionEntities.cs
@@ -2,7 +2,7 @@
  * Purpose:     Represents the various opinion entities in our app.
  * Created:     17th February 2015
  * Author:      Gary Fernie
- * Exposes:     Opinion, TweetOpinion, CountryOpinion, WorldSubjectOpinion
+ * Exposes:     Opinion, TweetOpinion, CountryOpinion, WorldSubjectOpinion, SubjectTweets
  * 
  * Description: - Various classes to represent entities.
  *              - Models various opinion states.
@@ -34,5 +34,10 @@ namespace GroupA.FolksOpinion.UI.Models
     {
         public string Subject { get; set; }
         public IEnumerable<CountryOpinion> CountryOpinions { get; set; }
+    }
+
+    public class SubjectTweets
+    {
+        
     }
 }
