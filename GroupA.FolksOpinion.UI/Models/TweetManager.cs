7b6fe864522a94be808757fa34fa57498e8aec1e
Gary Fernie <g@ryfernie.me>
2015-03-25 11:29:53 +0000

    Stub TweetManager

--- GroupA.FolksOpinion.UI/Models/TweetManager.cs
+++ GroupA.FolksOpinion.UI/Models/TweetManager.cs
@@ -5,7 +5,30 @@ using System.Web;
 
 namespace GroupA.FolksOpinion.UI.Models
 {
-    public class TweetManager
+    public partial class TweetManager
     {
+        public SubjectTweets SubjectTweets { get; protected set; }
+
+        public TweetManager(string subject)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected TweetManager() { }
+
+        protected SubjectTweets GetTweets(string subject)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RetrieveFromApi()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public partial class TweetManager
+    {
+
     }
 }
