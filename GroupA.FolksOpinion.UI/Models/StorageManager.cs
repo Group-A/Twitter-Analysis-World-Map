da61b3ba477b217ebb16464884004a190fac1051
Gary Fernie <g@ryfernie.me>
2015-03-25 15:02:20 +0000

    Add StorageManager class

---
+++ GroupA.FolksOpinion.UI/Models/StorageManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupA.FolksOpinion.UI.Models
+{
+    public class StorageManager : IFolksOpinionStorage
+    {
+        protected IFolksOpinionStorage Store { get; set; }
+
+        public void AddTweetOpinion(TweetOpinion tweetOpinion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TweetOpinion GetTweetOpinion(string tweetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSubjectTweets(SubjectTweets subjectTweets)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SubjectTweets GetSubjectTweets(string subject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
